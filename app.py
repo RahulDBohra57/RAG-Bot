9962082e5dc27d068f9e96646ee0e465faf70625
Rahul Bohra <rahulbohra57@gmail.com>
2026-01-22 15:46:52 +0530

    history added

--- app.py
+++ app.py
@@ -29,6 +29,9 @@ st.text(tips)
 st.sidebar.markdown('# :red[ UPLOAD YOUR FILE]')
 st.sidebar.subheader("Upload PDF file only")
 pdf_file = st.sidebar.file_uploader("Upload here",type=["pdf"])
+
+MAX_TURNS = 5
+
 if pdf_file:
     st.sidebar.success("File uploaded successfully")
     
@@ -43,16 +46,38 @@ if pdf_file:
     vector_store = FAISS.from_texts(chunks,embedding_model)
     retriver = vector_store.as_retriever(search_kwargs={'k':3})
     
+    def get_chat_history(exclude_latest=True):
+        history = ""
+        
+        messages = st.session_state.history
+        
+        # Exclude last user message (current query)
+        if exclude_latest and len(messages) > 0:
+            messages = messages[:-1]
+        
+        trimmed_history = st.session_state.history[-(MAX_TURNS * 2):]
+        
+        for msg in st.session_state.history:
+            history += f"{msg['role'].upper()}: {msg['text']}\n"
+        return history
+    
     def generate_content(query):
         # Step 3: Retrieval (R)
         retrived_docs = retriver.invoke(query)
         context = '\n'.join([d.page_content for d in retrived_docs])
         
+        chat_history = get_chat_history(exclude_latest=True)
+        
         # Step 4: Augmenting (A)
         augmented_prompt = f'''
-        <Role> You are a helpful assistant using RAG.
+        <Role> You are a helpful assistant using Conversational RAG.
         <Goal> Answer the questions asked by the user. Here is the question {query}
-        <Context> Here are the documnets retrieved from the vector Database to support the answer which you have to generate: {context}
+        <Context>
+        * Here are the documnets retrieved from the vector Database to support the answer which you have to generate: {context}
+        * Here is the conversation history {chat_history}
+        <Instructions>
+        - User conversation history to understand follow-up questions.
+        - Answer only from the provided context.
         '''
         
         # Step 5: Generate (G)
@@ -79,6 +104,11 @@ if pdf_file:
     # Start the conversation and append the output and query in history
     
     if user_query and send:
-        st.session_state.history.append({'role':'user','text':user_query})
-        st.session_state.history.append({'role':'bot','text':generate_content(user_query)})
+        st.session_state.history.append({'role':'user',
+                                         'text':user_query})
+        
+        bot_response = generate_content(user_query)
+        
+        st.session_state.history.append({'role':'bot',
+                                         'text':bot_response})
         st.rerun()
